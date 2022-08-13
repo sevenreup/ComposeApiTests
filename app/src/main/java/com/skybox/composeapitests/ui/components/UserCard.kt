d4a28b3e43d44f1ccc2b1e037d55d499f113bf79
Christopher Phiri <chris7phiri@gmail.com>
2022-08-13 18:02:28 +0200

    user card changes

--- app/src/main/java/com/skybox/composeapitests/ui/components/UserCard.kt
+++ app/src/main/java/com/skybox/composeapitests/ui/components/UserCard.kt
@@ -1,12 +1,15 @@
 package com.skybox.composeapitests.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,16 +19,36 @@ import com.skybox.composeapitests.ui.theme.ComposeApiTestsTheme
 @Composable
 fun UserCard(user: User) {
     Card(modifier = Modifier.fillMaxWidth()) {
-        Column(modifier = Modifier
-            .padding(12.dp)
-            .fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth()
+        ) {
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "${user.name} ${user.surname}, ",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Box(
+                    modifier = Modifier.background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(50.dp)
+                    )
+                ) {
+                    Text(
+                        text = "${user.age}",
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .padding(horizontal = 12.dp),
+                        style = MaterialTheme.typography.labelSmall.copy(color = MaterialTheme.colorScheme.onPrimary)
+                    )
+                }
+            }
             Text(
-                text = "${user.name} ${user.surname}",
-                style = MaterialTheme.typography.labelLarge,
+                text = user.city,
+                style = MaterialTheme.typography.labelMedium,
                 modifier = Modifier.fillMaxWidth()
             )
-            Text(text = "${user.age}")
-            Text(text = user.city)
         }
     }
 }
